6353bcbeecd022d321ba6d6dd7e4a9317c8ec806
Josh <josh@bonnick.dev>
2023-08-17 21:35:29 +0100

    refactor tests

--- tests/Feature/Components/ComponentFakerTest.php
+++ tests/Feature/Components/ComponentFakerTest.php
@@ -38,16 +38,6 @@ test('default entries do not return null', function () {
     }
 });
 
-it('uses methods added to config first', function () {
-    expect(TextInput::make('test')->fake())->not->toEqual('::test::');
-
-    config()->set('filament-faker.fakes', [
-        TextInput::class => fn () => '::test::',
-    ]);
-
-    expect(TextInput::make('test')->fake())->toEqual('::test::');
-});
-
 test('value is still returned when exception is thrown', function () {
     class TestField extends Field
     {
@@ -66,78 +56,10 @@ it('handles invalid options field', function () {
         ->toBeString();
 });
 
-it('does not execute slow methods listed in config file', function () {
-    config()->set('filament-faker.slow_faker_methods', ['safeEmail']);
-
-    expect(TextInput::make('safe_email')->fake())
-        ->not
-        ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/')
-        ->and(TextInput::make('email')->fake())
-        ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/');
-
-});
-
 it('can use a faker method if it exists', function () {
     expect(TextInput::make('safe_email')->fake())
         ->toBeString()
         ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/');
-});
-
-it('can disable the usage of faking by component name', function () {
-    expect(TextInput::make('safe_email')->fake())
-        ->toBeString()
-        ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/');
-
-    config()->set('filament-faker.use_component_names_for_fake', false);
-
-    expect(TextInput::make('safe_email')->fake())
-        ->toBeString()
-        ->not
-        ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/');
-});
-
-it('can disable the usage of faking by component name by chaining', function () {
-    $form = PostResource::faker()->getForm()->schema([
-        TextInput::make('safe_email'),
-    ]);
-
-    $data = $form->faker()->shouldFakeUsingComponentName(false)->fake();
-
-    expect($data['safe_email'])
-        ->not
-        ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/');
-
-    $data = $form->faker()->shouldFakeUsingComponentName(true)->fake();
-
-    expect($data['safe_email'])
-        ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/');
-
-    $data = PostResource::faker()->shouldFakeUsingComponentName(false)->fake();
-
-    expect($data['safe_email'])
-        ->not
-        ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/');
-});
-
-it('can disable the usage of faking by component name with method', function () {
-    expect(TextInput::make('safe_email')->fake())
-        ->toBeString()
-        ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/')
-        ->and($component = ComponentWithoutFakingFromNames::make('safe_email'))
-        ->toHaveMethod('shouldFakeUsingComponentName')
-        ->and($component->fake())
-        ->toBeString()
-        ->not
-        ->toMatch('/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/');
-
-});
-
-test('mutate fake method is a priority over faker method', function () {
-    expect($component = MutatedComponent::make('phone_number'))
-        ->toHaveMethod('mutateFake')
-        ->and($component->fake())
-        ->toBeString()
-        ->toEqual('::phone::');
 });
 
 it('uses option value even when faker method is available', function () {
