931882b45db3350795cfb68a610235933f273fef
Josh <josh@bonnick.dev>
2023-08-17 20:38:03 +0100

    add test

--- tests/Feature/Components/ComponentFormattingTest.php
+++ tests/Feature/Components/ComponentFormattingTest.php
@@ -1,6 +1,10 @@
 <?php
 
+use Carbon\Carbon;
+use Carbon\Exceptions\InvalidFormatException;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
+use FilamentFaker\Tests\TestSupport\Resources\PostResource;
 
 it('respects formatStateUsing', function () {
     $component = TextInput::make('email')->formatStateUsing(fn (string $state) => str($state)->wrap('<b>')->toString());
@@ -8,4 +12,24 @@ it('respects formatStateUsing', function () {
     expect($component->faker()->fake())
         ->toBeString()
         ->toContain('<b>');
+});
+
+it('returns a formatted date', function () {
+    $datepicker = PostResource::faker()->getForm()->schema([
+        DatePicker::make('published_at')
+            ->label('Published Date')
+            ->nullable()
+            ->default('31-12-2000')
+            ->format('Y-m-d'),
+    ]);
+
+    $date = $datepicker->fake()['published_at'];
+
+    expect($carbon = Carbon::parse($date))
+        ->not
+        ->toThrow(InvalidFormatException::class)
+        ->and($carbon->isValid())
+        ->toBeTrue()
+        ->and($date)
+        ->toEqual('2000-12-31');
 });
