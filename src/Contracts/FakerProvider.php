167f240c26975654c81e29b9c3556ccae74b0a6a
Josh <josh@bonnick.dev>
2023-08-17 17:00:13 +0100

    refactor

--- src/Contracts/FakerProvider.php
+++ src/Contracts/FakerProvider.php
@@ -4,11 +4,29 @@ declare(strict_types=1);
 
 namespace FilamentFaker\Contracts;
 
+use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\Field;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\KeyValue;
+use Filament\Forms\Components\TagsInput;
 
 interface FakerProvider
 {
     public function withOptions(Field $component): mixed;
 
     public function defaultCallback(Field $component): string;
+
+    public function withSuggestions(TagsInput $component): array;
+
+    public function date(): string;
+
+    public function file(FileUpload $upload): string;
+
+    public function keyValue(KeyValue $component): array;
+
+    public function color(ColorPicker $color): string;
+
+    public function html(): string;
+
+    public function checkbox(): bool;
 }
