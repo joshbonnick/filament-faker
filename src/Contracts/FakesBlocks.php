06cf16f29080f6365f87699daf92489f26a62e82
Josh <josh@bonnick.dev>
2023-08-17 22:03:39 +0100

    wip

--- src/Contracts/FakesBlocks.php
+++ src/Contracts/FakesBlocks.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace FilamentFaker\Contracts;
 
 use Closure;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
 
 interface FakesBlocks
 {
@@ -16,4 +18,9 @@ interface FakesBlocks
     public function shouldFakeUsingComponentName(bool $should = true): static;
 
     public function mutateFake(Closure $callback): static;
+
+    /**
+     * @param  class-string<Factory<Model>>|null  $factory
+     */
+    public function withFactory(string $factory = null): static;
 }
