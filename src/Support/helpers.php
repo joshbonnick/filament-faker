14a0590b0a46b89096bde5badb893eca6ed1e226
Josh <josh@bonnick.dev>
2023-08-18 19:31:38 +0100

    wip

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -7,6 +7,8 @@ use Closure;
 if (! function_exists('callOrReturn')) {
     /**
      * Execute a Closure or return provided data.
+     *
+     * @param mixed[] $args
      */
     function callOrReturn(mixed $data, ...$args): mixed
     {
