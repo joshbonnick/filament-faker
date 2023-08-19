90144a4533329b7982ad1d3b8253171ceb961276
Josh <josh@bonnick.dev>
2023-08-19 18:17:24 +0100

    Fix test

--- tests/Feature/Support/FactoryTest.php
+++ tests/Feature/Support/FactoryTest.php
@@ -1,17 +1,22 @@
 <?php
 
+use Faker\Provider\Text;
 use Filament\Forms\Components\TextInput;
 use FilamentFaker\Tests\TestSupport\Resources\PostResource;
 
 test('configure is applied before data is generated', function(){
-    TextInput::configureUsing(function(TextInput $component){
+    class ConfiguredComponent extends TextInput {
+
+    }
+
+    ConfiguredComponent::configureUsing(function(TextInput $component){
        $component->name('::test::')->afterStateHydrated(function(){
            return '::value::';
        });
     });
 
     $input = PostResource::faker()->getForm()->schema([
-        TextInput::make('safe_email')
+        ConfiguredComponent::make('safe_email')
     ]);
 
     expect($input->fake())->toEqual(['::test::' => '::value::']);
