468e94e2bce42fdba7a6980171a1754963b03818
Josh <josh@bonnick.dev>
2023-08-20 16:41:54 +0100

    add tests

---
+++ tests/Feature/Fakers/Forms/FormOnlyFieldsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Filament\Forms\Components\TextInput;
+
+it('returns only fields in only fields array', function () {
+    $form = mockForm()->schema([
+        TextInput::make('foo'),
+        TextInput::make('bar'),
+        TextInput::make('baz'),
+    ]);
+
+    $fake = $form->faker()->onlyFields('foo', 'bar')->fake();
+    expect(array_keys($fake))->toEqual(['foo', 'bar']);
+});
