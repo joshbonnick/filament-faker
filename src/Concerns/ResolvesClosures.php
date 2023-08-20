9c391fceafeffe1ba812a2c2f6aeaeefcbb0e9f4
Josh <josh@bonnick.dev>
2023-08-20 04:01:56 +0100

    Refactoring

---
+++ src/Concerns/ResolvesClosures.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FilamentFaker\Concerns;
+
+use Illuminate\Support\Facades\App;
+
+trait ResolvesClosures
+{
+    /**
+     * @template TReturnType
+     *
+     * @param  TReturnType|callable(): TReturnType  $callback
+     * @param  array<class-string|string, object>  $parameters
+     * @return ?TReturnType
+     */
+    protected function resolveOrReturn(mixed $callback, array $parameters = []): mixed
+    {
+        if (is_callable($callback)) {
+            return $this->resolveOrReturn(
+                callback: App::call($callback, $parameters = [...$this->injectionParameters(), ...$parameters]),
+                parameters: $parameters
+            );
+        }
+
+        return $callback;
+    }
+
+    /**
+     * @return array<class-string|string, object>
+     */
+    abstract protected function injectionParameters(): array;
+}
--- src/Fakers/FilamentFaker.php
+++ src/Fakers/FilamentFaker.php
@@ -5,37 +5,14 @@ declare(strict_types=1);
 namespace FilamentFaker\Fakers;
 
 use FilamentFaker\Concerns\InteractsWithFactories;
+use FilamentFaker\Concerns\ResolvesClosures;
 use FilamentFaker\Concerns\ResolvesFakerInstances;
 use FilamentFaker\Concerns\TransformsFakes;
-use Illuminate\Support\Facades\App;
 
 abstract class FilamentFaker
 {
     use InteractsWithFactories;
     use ResolvesFakerInstances;
     use TransformsFakes;
-
-    /**
-     * @template TReturnType
-     *
-     * @param  TReturnType|callable(): TReturnType  $callback
-     * @param  array<class-string|string, object>  $parameters
-     * @return ?TReturnType
-     */
-    protected function resolveOrReturn(mixed $callback, array $parameters = []): mixed
-    {
-        if (is_callable($callback)) {
-            return $this->resolveOrReturn(
-                callback: App::call($callback, $parameters = [...$this->injectionParameters(), ...$parameters]),
-                parameters: $parameters
-            );
-        }
-
-        return $callback;
-    }
-
-    /**
-     * @return array<class-string|string, object>
-     */
-    abstract protected function injectionParameters(): array;
+    use ResolvesClosures;
 }
